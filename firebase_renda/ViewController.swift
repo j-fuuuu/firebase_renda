d1e1cdddef608f5b4faef6231000e697ea0272a8
kumi fujii <kuum9393@gmail.com>
2022-09-02 08:55:06 +0900

    Initial Commit

---
+++ firebase_renda/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firebase_renda
+//
+//  Created by 藤井玖光 on 2022/09/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
